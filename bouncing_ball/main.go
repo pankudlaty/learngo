507aab8ae79bd080936c3c50826205f50627440e
Pawel <pkownacki90@gmail.com>
2022-03-18 11:43:07 +0100

    finished bb

--- bouncing_ball/main.go
+++ bouncing_ball/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
 
 func main() {
 	const (
@@ -10,21 +15,51 @@ func main() {
 		cellBall  = '⚾'
 		cellEmpty = ' '
 	)
-	var cell rune
+	var (
+		px, py int
+		vx, vy = 1, 1
+		cell   rune
+	)
+
 	board := make([][]bool, width)
 	for row := range board {
 		board[row] = make([]bool, height)
 	}
-	board[0][0] = true
-	for y := range board[0] {
-		for x := range board {
-			cell = cellEmpty
-			if board[x][y] {
-				cell = cellBall
-			}
-			fmt.Print(string(cell), " ")
+	screen.Clear()
+	for i := 0; i < 1200; i++ {
+		board[px][py] = true
+		px += vx
+		py += vy
 
+		if px <= 0 || px >= width-1 {
+			vx *= -1
 		}
-		fmt.Println()
+		if py <= 0 || py >= height-1 {
+			vy *= -1
+		}
+		for y := range board[0] {
+			for x := range board {
+				board[x][y] = false
+			}
+		}
+		board[px][py] = true
+		buf := make([]rune, 0, width*height)
+		buf = buf[:0]
+
+		for y := range board[0] {
+			for x := range board {
+				cell = cellEmpty
+				if board[x][y] {
+					cell = cellBall
+				}
+				//fmt.Print(string(cell), " ")
+				buf = append(buf, cell, ' ')
+			}
+			//fmt.Println()
+			buf = append(buf, '\n')
+		}
+		screen.MoveTopLeft()
+		fmt.Print(string(buf))
+		time.Sleep(time.Second / 20)
 	}
 }
